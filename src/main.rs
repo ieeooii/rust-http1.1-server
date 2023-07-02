1b958611452cc08b946209ef9eb77fc05482fe56
CLOwinnie <winnie@clo3d.com>
2023-07-03 01:14:29 +0900

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    let get = Method::GET("abcd".to_string());
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -15,3 +20,27 @@ impl Server {
         println!("Listening on {}", self.addr);
     }
 }
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET(String),
+    POST(u64),
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+ */
