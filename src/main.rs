bbbd6b31c0bb11c00579de615f2cf0d0996d94f3
CLOwinnie <winnie@clo3d.com>
2023-07-13 22:31:14 +0900

    code module -ing

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,46 @@
 fn main() {
-    let get = Method::GET("abcd".to_string());
-    let delete = Method::DELETE;
-    let post = Method::POST;
-    let put = Method::PUT;
-
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let server = server::Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
     }
 }
 
-struct Request {
-    path: String,
-    query_string: Option<u8>,
-    method: Method,
-}
+mod http {
+    mod requrest {
+        struct Request {
+            path: String,
+            query_string: Option<u8>,
+            method: method::Method,
+        }
+    }
 
-enum Method {
-    GET(String),
-    POST(u64),
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
+    mod method {
+        enum Method {
+            GET(String),
+            POST(u64),
+            PUT,
+            DELETE,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
 }
-
 /*
 GET /user?id=10 HTTP/1.1\r\n
 HEADERS \r\n
