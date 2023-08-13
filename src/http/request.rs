c7c2fdca242a9c182966baa93a06cee40c269879
CLOwinnie <winnie@clo3d.com>
2023-08-13 22:58:55 +0900

    as keyword & [..]

--- src/http/request.rs
+++ src/http/request.rs
@@ -12,32 +12,9 @@ HEADERS \r\n
 BODY
 */
 
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
-        unimplemented!()
-    }
-}
-
 impl TryFrom<&[u8]> for Request {
     type Error = String;
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        buf.encrypt();
-        unimplemented!()
-    }
-}
-
-trait Encrypt {
-    fn encrypt(&self) -> Self;
-}
-
-impl Encrypt for String {
-    fn encrypt(&self) -> Self {
-        unimplemented!()
-    }
-}
-
-impl Encrypt for &[u8] {
-    fn encrypt(&self) -> Self {
         unimplemented!()
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -20,7 +23,11 @@ impl Server {
                     let mut buffer = [1, 2, 3, 4];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
