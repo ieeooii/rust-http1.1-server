c2b8eba69857f6e794daf0475f351517e91d135d
CLOwinnie <winnie@clo3d.com>
2023-08-13 22:07:37 +0900

    trait:try_from

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
@@ -9,4 +10,17 @@ pub struct Request {
 GET /user?id=10 HTTP/1.1\r\n
 HEADERS \r\n
 BODY
- */
+*/
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
