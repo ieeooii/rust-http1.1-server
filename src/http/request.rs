36795e48523ae278333255b658e966bc94ba3532
CLOwinnie <winnie@clo3d.com>
2023-08-31 23:36:27 +0900

    option result

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,7 +2,6 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
-use std::str::Utf8Error;
 use std::str::{self, Utf8Error};
 
 pub struct Request {
@@ -19,19 +18,35 @@ BODY
 impl TryFrom<&[u8]> for Request {
     type Error = String;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...Headers...
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request: &str = str::from_utf8(buf)?;
+
+        // match get_next_word(request) {
+        //     Some((method, request)) => {}
+        //     None => return Err(ParseError::InvalidRequest),
+        // }
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(_, ParseError::Invalidprotocol);
+        }
+
         unimplemented!()
     }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' ' {
+        if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i + 1..]));
         }
     }
+
+    None
 }
 
 pub enum ParseError {
