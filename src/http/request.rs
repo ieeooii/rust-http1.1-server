3f9edcc66f2b7b8b18ded0afb611fa7cbeb21c9f
CLOwinnie <winnie@clo3d.com>
2023-10-15 00:15:53 +0900

    lifetime part1

--- src/http/request.rs
+++ src/http/request.rs
@@ -5,8 +5,8 @@ use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str::{self, Utf8Error};
 
 pub struct Request {
-    path: String,
-    query_string: Option<u8>,
+    path: &str,
+    query_string: Option<&str>,
     method: Method,
 }
 /*
@@ -16,7 +16,7 @@ BODY
 */
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1\r\n...Headers...
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
@@ -33,28 +33,16 @@ impl TryFrom<&[u8]> for Request {
         let method: Method = method.parse()?;
 
         let mut query_string = None;
-
-        match path.find('?') {
-            Some(i) => {
-                query_string = Some(&path[i + 1..]);
-                path = &path[..i];
-            }
-            None => {}
-        }
-
-        let q = path.find('?');
-        if q.is_some() {
-            let i = q.unwrap();
-            query_string = Some(&path[i + 1..]);
-            path = &path[..i];
-        }
-
         if let Some(i) = path.find('?') {
             query_string = Some(&path[i + 1..]);
             path = &path[..i];
         }
 
-        unimplemented!()
+        Ok(Self {
+            path,
+            query_string,
+            method,
+        })
     }
 }
 
