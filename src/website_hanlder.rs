1f893bbdaabb918df8f261b071fd61f1ed741ce6
CLOwinnie <winnie@clo3d.com>
2023-11-20 00:08:36 +0900

    fix text

--- src/website_hanlder.rs
+++ src/website_hanlder.rs
@@ -8,7 +8,7 @@ impl Handler for WebsiteHanlder {
         match request.method() {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
                 _ => Response::new(StatusCode::NotFound, None),
             },
             _ => Response::new(StatusCode::NotFound, None),
